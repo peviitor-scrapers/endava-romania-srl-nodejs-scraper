ab068edd85922c79c33a57e7c6fcf9625d8686d1
sebiboga <sebitestb@gmail.com>
2026-07-03 20:19:02 +0300

    Fix E2E test: add missing companyConfig import

--- tests/e2e/scraper.test.js
+++ tests/e2e/scraper.test.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fetch from 'node-fetch';
+import companyConfig from '../../config/company.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.resolve(__dirname, '../../.env.local') });
